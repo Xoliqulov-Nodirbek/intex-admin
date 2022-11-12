993eeb3400ac19acd14233b964b713ffdcf9ccfb
Nurillo18 <suyunboyevnurillo18@gmail.com>
2022-11-12 09:29:00 +0500

    Finish navbar and entering Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,8 +2,11 @@ import React from 'react'
 
 export default function Header() {
   return (
-    <div>
-        
+    <div className='bg-white w-full py-headerPaddingTopBottom px-headerPaddingX border-b-2'>
+        <input id='search-inp' className='w-headerInputWidth outline-none py-2.5 pl-headerInpPl bg-headerInpBg rounded-xl' type="text" placeholder='Поиск' />
+        <div>
+          
+        </div>
     </div>
   )
 }
--- src/components/Sitebar/SiteBar.tsx
+++ src/components/Sitebar/SiteBar.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import Logo from "../../Assets/Images/NavbarImgs/Site-logo.svg"
-import { NavLink, Router } from "react-router-dom"
+import { NavLink,Link } from "react-router-dom"
 import DropIcon from "../../Assets/Images/NavbarImgs/dropIcon.svg"
 import { ProductIcon, CallIcon, OrderICon, ExitICon, SettingsIcon } from "../../Assets/Images/NavbarImgs/NavBarIcons"
 
@@ -8,6 +8,7 @@ export default function SiteBar() {
   const [innnerActice, setInnnerActice] = useState(false)
   const [categoryActive, setCategoryActive] = useState(false)
   const [atributActive, setAtributActive] = useState(false)
+  const [navBarDrop, setNavBarDrop] = useState(false)
   function handActive() {
     setInnnerActice(true)
     setCategoryActive(false)
@@ -26,47 +27,64 @@ export default function SiteBar() {
     setInnnerActice(false)
   }
 
+  function delteInnerlink() {
+    setAtributActive(false)
+    setCategoryActive(false)
+    setInnnerActice(false)
+  }
+  function openDropDown() {
+    setNavBarDrop(!navBarDrop)
+  }
+
 
   return (
-    <div className='bg-white w-sitebarWidth h-screen'>
+    <div className='bg-white w-sitebarWidth h-screen relative'>
       <div className='pb-8  px-5'>
         <div className='py-8 border-b-2'>
-          <img src={Logo} alt="Site Logo" width={180} height={17} />
+          <Link to={'/'}>
+            <img src={Logo} alt="Site Logo" width={180} height={17} />
+          </Link>
         </div>
       </div>
       <div className=''>
-        <NavLink className="py-4 flex relative items-center px-6 text-sm leading-lead font-medium text-navBarColor" to={'/'}>
+        <NavLink onClick={() => {
+          delteInnerlink()
+          openDropDown()
+        }} className={`py-4 opacity-80 flex relative items-center px-6 text-sm leading-lead font-medium text-navBarColor`} to={'/'}>
           <ProductIcon />
           <span className='ml-navBarIcon'>Продукты</span>
-          <img className='absolute right-6' src={DropIcon} alt="Drop IMg" width={10} height={9} />
+          <img className={`absolute right-6 duration-200 -rotate-90 ${navBarDrop ? "rotate-0" : ""}`} src={DropIcon} alt="Drop IMg" width={10} height={9} />
         </NavLink>
-        <ul>
-          <li> 
-            <NavLink onClick={handActive} className={`${innnerActice ? "anterActive" : ""} py-2.5 pl-14 block font-normal text-navBarColor text-sm`} to={'listProduct'}>Список продуктов</NavLink>
+        <ul className={`h-0 overflow-hidden opacity-0 duration-500 ${navBarDrop ? "h-auto overflow-auto opacity-100" : ""}`}>
+          <li>
+            <NavLink onClick={handActive} className={`${innnerActice ? "anterActive" : ""} py-2.5 opacity-80 pl-14 block font-normal text-navBarColor text-sm`} to={'listProduct'}>Список продуктов</NavLink>
           </li>
           <li>
-            <NavLink onClick={handCategory} className={`${categoryActive ? "anterActive" : ""} py-2.5 pl-14 block font-normal text-navBarColor text-sm`} to={'category'}>Категории</NavLink>
+            <NavLink onClick={handCategory} className={`${categoryActive ? "anterActive" : ""} py-2.5 opacity-80 pl-14 block font-normal text-navBarColor text-sm`} to={'category'}>Категории</NavLink>
           </li>
           <li>
-            <NavLink onClick={handAtribut} className={`${atributActive ? "anterActive" : ""} py-2.5 pl-14 block font-normal text-navBarColor text-sm  `} to={'atribut'}>Атрибуты</NavLink>
+            <NavLink onClick={handAtribut} className={`${atributActive ? "anterActive" : ""} py-2.5 opacity-80 pl-14 block font-normal text-navBarColor text-sm  `} to={'atribut'}>Атрибуты</NavLink>
           </li>
         </ul>
-        <NavLink className="py-4 flex items-center px-6 text-sm leading-lead font-medium text-navBarColor" to={'/order'}>
+        <NavLink onClick={delteInnerlink} className="py-4 opacity-80 flex items-center px-6 text-sm leading-lead font-medium text-navBarColor" to={'/order'}>
           <OrderICon />
           <span className='ml-navBarIcon'>Заказы</span>
         </NavLink>
-        <NavLink className="py-4 flex items-center px-6 text-sm leading-lead font-medium text-navBarColor" to={'/callBack'}>
+        <NavLink onClick={delteInnerlink} className="py-4 opacity-80 flex items-center px-6 text-sm leading-lead font-medium text-navBarColor" to={'/callBack'}>
           <CallIcon />
           <span className='ml-navBarIcon'>Обратный связ</span>
         </NavLink>
-        <NavLink className="py-4 flex items-center px-6 text-sm leading-lead font-medium text-navBarColor" to={'/settings'}>
+        <NavLink onClick={delteInnerlink} className="py-4 opacity-80 flex items-center px-6 text-sm leading-lead font-medium text-navBarColor" to={'/settings'}>
           <SettingsIcon />
           <span className='ml-navBarIcon'>Настройки сайта</span>
         </NavLink>
-        <NavLink className="py-4 flex items-center px-6 text-sm leading-lead font-medium text-navBarColor" to={'/exit'}>
+        <NavLink onClick={delteInnerlink} className={` py-4 opacity-80 flex items-center px-6 text-sm leading-lead font-medium text-navBarColor`} to={'/exit'}>
           <ExitICon />
           <span className='ml-navBarIcon'>Выйти</span>
         </NavLink>
+      </div>
+      <div className='text-sm leading-lead pl-5 pb-5 text-navBarColor w-supportWidth absolute bottom-0'>
+        <p>Developed by <span className='text-supportColor'>Support IT Solution </span></p> 
       </div>
     </div>
   )
